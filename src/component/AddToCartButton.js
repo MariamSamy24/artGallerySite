287fef1f193ae5228918f4d40c5182e81ab1e151
Mariam <mariam@cdws.travel>
2024-10-18 01:44:19 +0300

    update cart design

--- src/component/AddToCartButton.js
+++ src/component/AddToCartButton.js
@@ -1,28 +1,42 @@
-import React, { useContext } from 'react';
-import { CartContext } from '../context/CartContext'; // Import the context
+import React, { useContext, useState, useEffect } from 'react';
+import { CartContext } from '../context/CartContext'; 
 import { toast } from 'react-toastify';
 
 const AddToCartButton = ({ product }) => {
-  const { addToCart } = useContext(CartContext); // Get addToCart function from context
+  const { cart, addToCart } = useContext(CartContext); 
+  const [isDisabled, setIsDisabled] = useState(false);
+
+
+
+  useEffect(() => {
+    const existingProduct = cart.find((item) => item.id === product.id);
+    if (existingProduct && existingProduct.quantity >= product.stock) {
+      setIsDisabled(true); 
+    } else {
+      setIsDisabled(false); 
+    }
+  }, [cart, product]);
 
   const handleAddToCart = () => {
-    addToCart(product);
-    toast.success(`${product.title} has been added to your cart!`);
+    if (!isDisabled) {
+      addToCart(product);
+      toast.success(`${product.title} has been added to your cart!`);
+    }
   };
 
   return (
     <button 
       onClick={handleAddToCart} 
-      disabled={product.stock === 0} 
+      disabled={isDisabled ||product.stock === 0} 
       style={{
         borderRadius: '10px',
         color: 'white',
         padding: '10px',
-        backgroundColor: product.stock === 0 ? 'grey' : '#088395',
-        cursor: product.stock === 0 ? 'not-allowed' : 'pointer',
+        backgroundColor: product.stock === 0 ? 'grey' : isDisabled ? "#e74c3c": '#088395',
+        cursor: isDisabled? 'not-allowed' : 'pointer',
       }}
     >
-      {product.stock === 0 ? 'Sold Out' : 'Add to Cart'}
+      {product.stock === 0 ? 'Sold Out' : isDisabled ? 'Max Reached' : 'Add to Cart'}
     </button>
   );
 };
