f021815107f870b1af21676d16d7a622acaa0d4b
Mariam <mariam@cdws.travel>
2024-10-23 21:33:05 +0300

    remove sendOrderConfirmationEmail

--- src/pages/OrderConfirmation/OrderConfirmationPage.js
+++ src/pages/OrderConfirmation/OrderConfirmationPage.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import './OrderConfirmationPage.css';
 
 function OrderConfirmationPage() {
-  debugger
+  
   const location = useLocation();
   const { orderId, total, paymentType } = location.state || {};
 
@@ -11,7 +11,7 @@ function OrderConfirmationPage() {
     <div className="order-confirmation">
       <h1>Order Confirmation</h1>
       <p>Thank you for your order!</p>
-      <p>Order ID: {orderId}</p>
+      <p>Order Number: {orderId}</p>
       <p>Total Amount: ${total}</p>
       <p>Payment Method: {paymentType}</p>
       <p>Your order is being processed and you will receive a confirmation email shortly.</p>
