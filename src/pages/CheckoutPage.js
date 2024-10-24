75e9d897416d0647b9e4e102b7ad78816d9b3339
NouraNabilMounir <noranabilmounir@gmail.com>
2024-10-24 18:34:20 +0200

    hosting

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -21,6 +21,7 @@ function CheckoutPage() {
     const fetchCartItems = async () => {
       try {
         const response = await axios.get('/api/cart');
+        console.log(response.data); // تسجيل استجابة API للتحقق من البيانات
         setCartItems(response.data);
       } catch (error) {
         console.error('Error fetching cart items:', error);
@@ -44,8 +45,6 @@ function CheckoutPage() {
   const handleCheckout = async (checkoutData) => {
     try {
       const response = await axios.post('/api/checkout', checkoutData);
-
-      // Handle successful checkout
       console.log('Checkout successful:', response.data);
     } catch (error) {
       setError(error.response.data.message);
@@ -54,13 +53,13 @@ function CheckoutPage() {
 
   return (
     <div>
-      {/* <h2>Checkout</h2>
+      <h2>Checkout</h2>
       <h3>User Information</h3>
       <p>Name: {user?.name}</p>
       <p>ID: {user?.id}</p>
-      <h3>Cart Items</h3> */}
+      <h3>Cart Items</h3>
       <ul>
-        {cartItems.map((item) => (
+        {(Array.isArray(cartItems) ? cartItems : []).map((item) => (
           <li key={item.id}>
             {item.product.name} - Quantity: {item.quantity}
           </li>
